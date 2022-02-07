9f91aebba41d663fd344358a3561231197b6da06
V-Donut <vikaevt123@gmail.com>
2022-02-07 23:56:30 +0300

    Фибоначчи

---
+++ fibonacci.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const fibonacci = num => {
+    const result = [0, 1];
+
+    for (let i = 2; i <= num; i++) {
+        let prevSum1 = result[i - 1];
+        let prevSum2 = result[i - 2];
+        result.push(prevSum1 + prevSum2);
+    }
+
+    console.log(result);
+    return result[num];
+};
+
+console.log(fibonacci(8));
