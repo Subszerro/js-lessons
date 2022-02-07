0bd9fdc67616b57b35b386cdd0b4c1b0f0f89b48
V-Donut <vikaevt123@gmail.com>
2022-02-07 23:31:38 +0300

    Поиск гласных

--- searchVowels.js
+++ searchVowels.js
@@ -3,6 +3,7 @@
 const searchVowels = function (str) {
     let counter = 0;
     const vowels = ['a', 'e', 'i', 'o', 'u'];
+    str = str.toLowerCase();
 
     for (let i = 0; i <= str.length; i++) {
         if (vowels.indexOf(str[i]) !== -1) {
