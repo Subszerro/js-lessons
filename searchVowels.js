29d05a8b65118735b59a21689c83c8705349fe9f
V-Donut <vikaevt123@gmail.com>
2022-02-07 23:29:06 +0300

    Поиск гласных

---
+++ searchVowels.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const searchVowels = function (str) {
+    let counter = 0;
+    const vowels = ['a', 'e', 'i', 'o', 'u'];
+
+    for (let i = 0; i <= str.length; i++) {
+        if (vowels.indexOf(str[i]) !== -1) {
+            counter++;
+        }
+    }
+
+    return counter;
+};
+
+console.log(searchVowels('hello'));
+console.log(searchVowels('world'));
